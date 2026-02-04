86eafe5f3e5e678bb588da89a3a620d9387d5696
JinqingZhengTju <hbumczjq@126.com>
2026-02-04 20:35:45 +0800

    refactor

--- code/mmrotate/mmrotate/datasets/__init__.py
+++ code/mmrotate/mmrotate/datasets/__init__.py
@@ -4,5 +4,7 @@ from .dota import DOTADataset  # noqa: F401, F403
 from .hrsc import HRSCDataset  # noqa: F401, F403
 from .pipelines import *  # noqa: F401, F403
 from .sar import SARDataset  # noqa: F401, F403
+from .dfgovd import DFGOVDDataset, DFGOVDCoarseDataset   # noqa: F401, F403
 
-__all__ = ['SARDataset', 'DOTADataset', 'build_dataset', 'HRSCDataset']
+__all__ = ['SARDataset', 'DOTADataset', 'build_dataset', 'HRSCDataset',
+           'DFGOVDDataset', 'DFGOVDCoarseDataset']
